fa319998f30ca719f73f5870248a9e4bbffb380c
Daniel Felicita <daniel.felicita@gmail.com>
2022-09-22 13:07:57 -0500

    Episode Listing

--- src/app/episode-list/episode-list.component.ts
+++ src/app/episode-list/episode-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiRestService, episode } from '../api-rest.service';
 
 @Component({
   selector: 'app-episode-list',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./episode-list.component.scss']
 })
 export class EpisodeListComponent implements OnInit {
+  episodeList: episode[] = [];
+  nextPage: string | undefined = "";
 
-  constructor() { }
+  constructor(private API: ApiRestService) { }
 
   ngOnInit(): void {
+    this.API.getEpisodeList().subscribe((response) => {
+      this.episodeList = response.results;
+      this.nextPage = response.info.next;
+    });
   }
 
+  onScroll() {
+    if(this.nextPage){
+      this.API.getEpisodeList(this.nextPage).subscribe((response) => {
+        this.episodeList = this.episodeList.concat(response.results);      
+        this.nextPage = response.info.next;
+      });
+    }
+  }
 }
