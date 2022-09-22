c49d051e5b90cd0e45bff0d473df0df4c2ea1265
Daniel Felicita <daniel.felicita@gmail.com>
2022-09-21 19:59:24 -0500

    Character Search

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiRestService, character } from '../api-rest.service';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from "rxjs/operators";
+
 
 @Component({
   selector: 'app-character-list',
@@ -9,10 +12,21 @@ import { ApiRestService, character } from '../api-rest.service';
 export class CharacterListComponent implements OnInit {
 
   characterList: character[] = [];
-  nextPage: string | undefined= "";
+  nextPage: string | undefined = "";
+  loading: boolean = false;
+  genderSearch: string = "";
+  statusSearch: string = "";
+  nameSearch: string = "";
+  nameSearchChanged: Subject<string> = new Subject<string>();
 
   constructor(private API: ApiRestService) {
-
+    this.nameSearchChanged.pipe(
+      debounceTime(400),
+      distinctUntilChanged())
+      .subscribe(value => {
+        this.nameSearch = value;
+        this.searchCharacter();
+      });
   }
 
   ngOnInit(): void {
@@ -23,10 +37,37 @@ export class CharacterListComponent implements OnInit {
   }
 
   onScroll() {
-    this.API.getCharacterList(this.nextPage).subscribe((response) => {
-      this.characterList = this.characterList.concat(response.results);      
+    if(this.nextPage){
+      this.API.getCharacterList(this.nextPage).subscribe((response) => {
+        this.characterList = this.characterList.concat(response.results);      
+        this.nextPage = response.info.next;
+      });
+    }
+  }
+
+  searchCharacter() {
+    this.loading = true;
+    let query = '';
+
+    if(this.nameSearch){
+      query += 'name='+this.nameSearch;
+    }
+
+    if(this.genderSearch){
+      query += this.nameSearch ? '&gender='+this.genderSearch : 'gender='+this.genderSearch;
+    }
+
+    if(this.statusSearch){
+      query += this.nameSearch || this.genderSearch ? '&status='+this.statusSearch : 'status='+this.statusSearch;
+    }
+
+    this.API.searchCharacter(query).subscribe((response) => {
+      console.log('query:', query, 'response:', response);
+      this.characterList = response.results;
       this.nextPage = response.info.next;
+      this.loading = false
     });
+    
   }
 
 }
